8f0a1f37ca4e8d380745d27080658c87074b0e46
Despina22 <stankovskadespina22@gmail.com>
2023-08-29 23:35:15 +0200

    feat: add header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [NavigationComponent, HeaderComponent],
   imports: [CommonModule, MatIconModule],
-  exports: [NavigationComponent],
+  exports: [NavigationComponent, HeaderComponent],
 })
 export class CoreModule {}
