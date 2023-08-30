d569e9151963d0c6994d86f0e30df61d3f55f774
Despina22 <stankovskadespina22@gmail.com>
2023-08-30 21:06:15 +0200

    feat: add games component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 import { EarningComponent } from './components/earning/earning.component';
+import { GamesComponent } from './components/games/games.component';
 
 @NgModule({
-  declarations: [EarningComponent],
+  declarations: [EarningComponent, GamesComponent],
   imports: [CommonModule, FeaturesRoutingModule],
-  exports: [EarningComponent],
+  exports: [EarningComponent, GamesComponent],
 })
 export class FeaturesModule {}
